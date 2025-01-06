8335b33b794f764d229edbb1a8b3bda35c1a3faf
Javier Barrera <js.barrerat1@uniandes.edu.co>
2025-01-06 15:24:44 -0500

    Add pagination of 5 posts to blogs view

--- blog/views.py
+++ blog/views.py
@@ -12,12 +12,16 @@ class BlogListView(ListView):
   template_name = 'blogs/list.html'
   context_object_name = 'blogs'
   ordering = ['-date_posted']
+  paginate_by = 5
 
+# TODO: Section of comments.
+# TODO: Comments should to be order by date_posted (oldest first).
 class BlogDetailView(DetailView):
   model = Blog
   template_name = 'blogs/detail.html'
   context_object_name = 'blog'
 
+# TODO: Add a list of all posts made by a blogger to the BloggerDetailView. Only show blog post date and name.
 class BloggerDetailView(DetailView):
   model = Blogger
   template_name = 'bloggers/detail.html'
